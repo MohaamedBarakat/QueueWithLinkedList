aa8414310086f25f781d3436937354f945e1917a
Mohamed Barakat <44622370+MohaamedBarakat@users.noreply.github.com>
2019-04-07 17:34:52 +0200

    Imp of queue with linked;ist

---
+++ QueueWithLinkedList/main.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+
+using namespace std;
+typedef int EleType;
+typedef struct node{
+EleType data;
+struct node*next;
+}Node;
+typedef Node *Queue;
+typedef struct {
+Node *Front;
+Node *Rear;
+}Queue_t;
+int main()
+{
+
+    return 0;
+}
+int Init(Queue q,EleType x){
+
+
+
+}
